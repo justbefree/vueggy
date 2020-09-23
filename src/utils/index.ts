c18e9c1950d9cd666b85cd8358e2ae24c9357576
Just be free <justbefree@126.com>
2020-09-23 14:25:49 +0800

    Correct method's name

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,19 +2,17 @@
 * @Author: Just be free
 * @Date:   2020-09-22 16:20:15
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-22 18:46:40
+* @Last Modified time: 2020-09-23 14:23:38
 * @E-mail: justbefree@126.com
 */
-export const capitalize = (str: string): string => {
+export const hyphenate = (str: string): string => {
   return String(str).replace(/\B([A-Z])/g, "-$1").toLowerCase();
 };
-export const camelize = (str: string, upperCaseFirstLetter: boolean = false): string => {
-  let ca = String(str).replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ""));
-  if (upperCaseFirstLetter) {
-    return ca.replace(/\b\w/g, (f) => f.toUpperCase());
-  } else {
-    return ca;
-  }
+export const camelize = (str: string): string => {
+  return String(str).replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ""));
+};
+export const capitalize = (str: string): string => {
+  return String(str).charAt(0).toUpperCase() + str.slice(1);
 };
 export const isString = (value: any): boolean => {
   return Object.prototype.toString.call(value) === "[object String]";
