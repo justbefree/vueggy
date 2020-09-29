bc34f60448dfbcd39cf77a086d4a221cf922c2e6
Just be free <justbefree@126.com>
2020-09-29 18:12:44 +0800

    first build

--- lib/vueggy.ts
+++
@@ -1,8 +0,0 @@
-/*
-* @Author: Just be free
-* @Date:   2020-09-23 11:35:40
-* @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-23 11:35:51
-* @E-mail: justbefree@126.com
-*/
-console.log("hi vueggy");
