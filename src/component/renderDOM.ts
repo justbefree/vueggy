a66edab3cb710ec73d6155518d15dfaf790635da
Just be free <justbefree@126.com>
2020-09-30 11:56:46 +0800

    Remove console

--- src/component/renderDOM.ts
+++ src/component/renderDOM.ts
@@ -2,7 +2,7 @@
 * @Author: Just be free
 * @Date:   2020-09-25 10:13:20
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-27 15:50:03
+* @Last Modified time: 2020-09-30 11:56:32
 * @E-mail: justbefree@126.com
 */
 
@@ -10,7 +10,6 @@ import { h, render, DefineComponent, AllowedComponentProps, VNode } from "vue";
 export function renderDOM(component: any, props: any, children: any) {
   const vnode: VNode = h(component, { ...props }, children);
   const dom = document.createElement("div");
-  console.log("renderDOM ", vnode, render(vnode, dom));
   render(vnode, dom);
   return vnode.component;
 }
