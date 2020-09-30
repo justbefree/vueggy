75e1c5dd044c2e42937abc23a8130954d026a37c
Just be free <justbefree@126.com>
2020-09-30 11:55:00 +0800

    Remove console

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 * @Author: Just be free
 * @Date:   2020-09-22 15:24:16
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-30 11:36:50
+* @Last Modified time: 2020-09-30 11:52:40
 * @E-mail: justbefree@126.com
 */
 import { version } from "../package.json";
@@ -15,12 +15,8 @@ import VgPopup from "./popup";
 const components = [VgFlex, VgFlexItem, VgSpin, VgPopup];
 
 const install = (app: any, options: any) => {
-  console.log("app", app);
   components.map(component => {
     if (component.componentName) {
-      // console.log((new component({})).constructor.name);
-      console.log(component.componentName);
-      // console.log(component, component.name, component.constructor.name);
       app.component(component.componentName, component);
     }
   });
@@ -28,5 +24,5 @@ const install = (app: any, options: any) => {
   app.config.isCustomElement = (tag: string) => tag.startsWith("vg-");
 };
 const Vueggy = { install, version };
-export { VgFlex, VgFlexItem, VgToast, VgSpin, VgPopup }
-export default Vueggy
+export { VgFlex, VgFlexItem, VgToast, VgSpin, VgPopup };
+export default Vueggy;
