34cff1224cd94c02d710d9aa1ec8c031971b5d7a
Just be free <justbefree@126.com>
2020-09-30 15:42:16 +0800

    Using global nextTick replace

--- src/toast/index.ts
+++ src/toast/index.ts
@@ -2,13 +2,13 @@
 * @Author: Just be free
 * @Date:   2020-09-23 16:16:39
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-30 15:30:41
+* @Last Modified time: 2020-09-30 15:41:31
 * @E-mail: justbefree@126.com
 */
 import VgToast from "./toast";
 import { isString } from "../utils";
 import { renderDOM } from "../component/renderDOM";
-import { ComponentInternalInstance } from "vue";
+import { nextTick } from "vue";
 export interface ToastOptions {
   message: string;
   position: "middle" | "bottom" | "top";
@@ -28,16 +28,17 @@ const Toast = (options: ToastOptions) => {
   ctx.setClosed(false);
   clearTimeout(ctx.timer);
   document.body.appendChild(ctx.getDomTree() as HTMLElement);
-  ctx.$nextTick(() => {
+  console.log("ctx = ", ctx);
+  nextTick(() => {
     ctx.setVisiable(true);
-    ctx.$el.removeEventListener("transitionend", removeDom, false);
+    ctx.getDomTree().removeEventListener("transitionend", removeDom, false);
     ~duration &&
       (ctx.timer = setTimeout(() => {
         if (ctx.closed) {
           return false;
         }
         ctx.close(() => {
-          ctx.$el.addEventListener("transitionend", removeDom, false);
+          ctx.getDomTree().addEventListener("transitionend", removeDom, false);
         });
       }, duration));
   });
