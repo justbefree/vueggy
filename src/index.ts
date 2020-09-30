57139a0f2def223792ef7c9ab7b03809c8cc0d8b
Just be free <justbefree@126.com>
2020-09-30 10:56:08 +0800

    log info

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 * @Author: Just be free
 * @Date:   2020-09-22 15:24:16
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-30 10:48:20
+* @Last Modified time: 2020-09-30 10:55:43
 * @E-mail: justbefree@126.com
 */
 import { version } from "../package.json";
@@ -13,6 +13,10 @@ import VgToast from "./toast";
 import VgSpin from "./spin";
 import VgPopup from "./popup";
 const components = [VgFlex, VgFlexItem, VgSpin, VgPopup];
+console.log(VgFlex.name);
+console.log(VgFlexItem.name);
+console.log(VgSpin.name);
+console.log(VgPopup.name);
 const install = (app: any, options: any) => {
   console.log("app", app);
   components.map(component => {
