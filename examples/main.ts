8d8c1e171e828e64a7507c4e8f5593349cda7e1b
Just be free <justbefree@126.com>
2020-09-30 10:18:00 +0800

    Add log info

--- examples/main.ts
+++ examples/main.ts
@@ -2,16 +2,18 @@
 * @Author: Just be free
 * @Date:   2020-09-21 17:37:50
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-23 16:12:54
+* @Last Modified time: 2020-09-30 10:12:24
 * @E-mail: justbefree@126.com
 */
 
-import { createApp } from "vue";
+// import { createApp } from "vue";
+import * as Vue from "vue";
+console.log("This is Vue3 ", Vue);
 import App from "./App.vue";
 import router from "./routes";
 import vueggy from "@";
 // console.log(vueggy);
-const app = createApp(App);
+const app = Vue.createApp(App);
 app.use(router);
 app.use(vueggy);
 app.mount("#example");
