39ea500e8766aae217faf5c30dde8fc3c78c5fb2
Just be free <justbefree@126.com>
2020-10-12 12:47:18 +0800

    Add VgCheckbox component

---
+++ src/checkbox/index.ts
@@ -0,0 +1,9 @@
+/*
+* @Author: Just be free
+* @Date:   2020-10-12 11:52:08
+* @Last Modified by:   Just be free
+* @Last Modified time: 2020-10-12 11:52:48
+* @E-mail: justbefree@126.com
+*/
+import Checkbox from "./checkbox";
+export default Checkbox;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 * @Author: Just be free
 * @Date:   2020-09-22 15:24:16
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-30 18:03:22
+* @Last Modified time: 2020-10-12 12:00:14
 * @E-mail: justbefree@126.com
 */
 import { version } from "../package.json";
@@ -13,11 +13,12 @@ import VgToast from "./toast";
 import VgSpin from "./spin";
 import VgPopup from "./popup";
 import VgIcon from "./icon";
+import VgCheckbox from "./checkbox";
 export interface VueggyConfigOptions {
   [propName: string]: any;
 }
 const configOptions = {} as VueggyConfigOptions;
-const components = [VgFlex, VgFlexItem, VgSpin, VgPopup, VgIcon];
+const components = [VgFlex, VgFlexItem, VgSpin, VgPopup, VgIcon, VgCheckbox];
 
 const install = (app: any, options: any) => {
   components.map(component => {
