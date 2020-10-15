3feb8a47e1777a48017765a2b1e223149a9fea55
Just be free <justbefree@126.com>
2020-10-15 14:32:16 +0800

    Add some functional methods

--- src/utils/dom/index.ts
+++ src/utils/dom/index.ts
@@ -2,7 +2,7 @@
 * @Author: Just be free
 * @Date:   2020-09-28 18:14:16
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-28 18:17:43
+* @Last Modified time: 2020-10-15 11:38:45
 * @E-mail: justbefree@126.com
 */
 
@@ -18,6 +18,50 @@ const remove = (dom: HTMLElement, className: string): void => {
   if (hasClass(dom, className)) {
     const reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
     dom.className = dom.className.replace(reg, " ");
+  }
+};
+// 获取scrollTop值
+export const getScrollTop = (dom: HTMLElement): number => {
+  if (dom && dom.nodeType && dom.nodeType === 9) {
+    return Math.max(document.documentElement.scrollTop, window.pageYOffset, document.body.scrollTop);
+  }
+  let t = dom.scrollTop;
+  while (dom.offsetParent) {
+    dom = dom.offsetParent as HTMLElement;
+    t += dom.scrollTop;
+  }
+  return t;
+};
+export interface OffsetValue {
+  top: number;
+  left: number;
+}
+export const getOffset = (element: HTMLElement): OffsetValue => {
+  let top = element.offsetTop;
+  let left = element.offsetLeft;
+  let current = element.offsetParent as HTMLElement;
+  while (current !== null) {
+    let translate = getElementsTranslate(current);
+    top += (current.offsetTop + translate.y);
+    left += (current.offsetLeft + translate.x);
+    current = current.offsetParent as HTMLElement;
+  }
+  return { top, left };
+};
+export interface TranslateVlue {
+  x: number;
+  y: number;
+}
+// 获取translateY的值
+export const getElementsTranslate = (element: HTMLElement): TranslateVlue => {
+  const style = window.getComputedStyle(element);
+  const transform = style.transform || style.webkitTransform;
+  if (transform !== "none") {
+    const translateY = transform.slice(7, transform.length - 1).split(", ")[5];
+    const translateX = transform.slice(7, transform.length - 1).split(", ")[4];
+    return { y: Number(translateY), x: Number(translateX) };
+  } else {
+    return { y: 0, x: 0 };
   }
 };
 export const removeClass = (dom: HTMLElement, className: string): void => {
