5bc5a09b9d583dcdb8ebc55fe754b82ec08e07ae
Just be free <justbefree@126.com>
2020-10-27 18:35:41 +0800

    Add types

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+/*
+* @Author: Just be free
+* @Date:   2020-10-27 16:26:57
+* @Last Modified by:   Just be free
+* @Last Modified time: 2020-10-27 16:42:16
+* @E-mail: justbefree@126.com
+*/
+interface Document {
+  [propName: string]: any;
+}
+// interface Window {
+//   [propName: string]: any;
+// }
