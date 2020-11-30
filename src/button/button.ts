7517da1acb61ed1ebf8b1cb1d48f1cda2a39c14e
Just be free <justbefree@126.com>
2020-11-30 15:57:20 +0800

    Add size prop for VgButton component

--- src/button/button.ts
+++ src/button/button.ts
@@ -2,13 +2,17 @@
 * @Author: Just be free
 * @Date:   2020-10-12 15:56:53
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-10-14 16:19:21
+* @Last Modified time: 2020-11-30 15:54:41
 * @E-mail: justbefree@126.com
 */
 import VueGgy, { mixins, props, Options } from "../component/VueGgy";
 import VgIcon from "../icon";
 import { h } from "vue";
 const Props = props({
+  size: {
+    type: String,
+    default: "default"
+  },
   type: {
     type: String,
     default: "default"
@@ -38,9 +42,15 @@ export default class VgButton extends mixins(VueGgy, Props) {
   validType(type: string): boolean {
     return ["default", "primary", "dashed"].indexOf(type) > -1;
   }
+  validSize(size: string): boolean {
+    return ["default", "large"].indexOf(size) > -1;
+  }
   render() {
     const slots = this.getSlots();
     let className = [];
+    if (this.validSize(this.size)) {
+      className.push(this.size);
+    }
     if (this.validType(this.type)) {
       className.push(`vg-button-${this.type}`);
     }
