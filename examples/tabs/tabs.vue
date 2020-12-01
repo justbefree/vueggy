b8f82ce676e37e038fd36aa033899532eee8693c
Just be free <justbefree@126.com>
2020-12-01 10:41:59 +0800

    Update VgTabs examples

--- examples/tabs/tabs.vue
+++ examples/tabs/tabs.vue
@@ -2,16 +2,27 @@
   <div class="tabs-view">
     <div id="container">
       <div class="box">
+        <vg-tabs v-model="active0">
+          <vg-tab-item title="tab111" name="111111">1</vg-tab-item>
+          <vg-tab-item title="tab222" name="22222">2</vg-tab-item>
+          <vg-tab-item title="tab333" name="33333">3</vg-tab-item>
+        </vg-tabs>
+      </div>
+      <div class="box">
         <vg-tabs v-model="active" @change="change">
-          <vg-tab-item :key="index" :title="'Tab' + index" v-for="index in 4">
+          <vg-tab-item :key="index" :name="'tab-' + index" :title="'Tab' + index" v-for="index in 4">
             content of tab {{ index }}
+          </vg-tab-item>
+          <vg-tab-item :key="`item—${index}`" :name="'item-' + index" :title="'Tab' + index" v-for="index in 4">
+            content of tab {{ `item-${index}` }}
           </vg-tab-item>
         </vg-tabs>
       </div>
       <div class="box">
-        <vg-tabs v-model="active2" @click="handleClick">
+        <vg-tabs v-model="active1" @click="handleClick">
           <vg-tab-item
             :key="index"
+            :name="'tab-' + index"
             :title="'Tab' + index"
             v-for="index in 8"
             :disabled="index === 4"
@@ -22,7 +33,7 @@
       </div>
       <div class="box">
         <vg-tabs v-model="active2">
-          <vg-tab-item :key="index" :title="'Tab' + index" v-for="index in 2">
+          <vg-tab-item :key="index" :name="'Tab-' + index" :title="'Tab' + index" v-for="index in 2">
             content of tab {{ index }}
           </vg-tab-item>
         </vg-tabs>
@@ -35,9 +46,10 @@
 import { Vue } from "vue-class-component";
 // import VueGgy from "../../src/component/VueGgy";
 export default class Tabs extends Vue {
-  public active = 1;
-  public active1 = 1;
-  public active2 = 1;
+  public active0 = "111111";
+  public active = "tab-1";
+  public active1 = "tab-1";
+  public active2 = "Tab-1";
   change(e: any) {
     this.Toast(`${e.value} changed`);
   }
