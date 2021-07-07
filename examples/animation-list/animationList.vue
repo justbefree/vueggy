0923b40f01f3b64738ae29adad4f7f445044d356
Just be free <justbefree@126.com>
2021-07-07 15:27:03 +0800

    docs: update examples

--- examples/animation-list/animationList.vue
+++ examples/animation-list/animationList.vue
@@ -2,7 +2,7 @@
   <div class="animation-list-view">
     <div id="container">
       <vg-button @click="filter">数组反转</vg-button>
-      <vg-animation-list :dataList="asyncData" :animation="animation">
+      <vg-animation-list>
         <vg-animation-list-item v-for="i in asyncData" :key="i.index">
           <!-- <div style="height: 100px;" :class="[i.index % 2 === 0 ? 'hide' : '']">这是第{{i.index}}个元素</div> -->
           <div style="height: 80px;" v-if="i.index === 5">
